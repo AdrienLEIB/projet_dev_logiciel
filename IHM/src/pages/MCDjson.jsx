68979ac8f76646b6e7b43830a0d2369ed94ea1ab
adrienleib <adrien.leib@ynov.com>
2020-06-07 19:46:17 +0200

    presentation update

--- IHM/src/pages/MCDjson.jsx
+++ IHM/src/pages/MCDjson.jsx
@@ -18,28 +18,28 @@ export class MCDjson extends Component {
             title2 : "Produit",
             description2 : {
               "stock": 50,
-              "price": 199,
-              "idmotherproduct":"5ec2b6194f85c619406e3c9f"
+              "price": 9999,
+              "idmotherproduct":"5ed7dd6b056c2e2cbcd42126"
             },
             title3: "Facture",
             description3: {
-                "paid":true,
-                "products":[{"id": "5eaae05b8a465626409884af", "price": 5, "qty": 5}],
-                "price":199
-              },
+              "paid":true,
+              "products":[{"id": "5ed7dfa7056c2e2cbcd4212d", "price": 5, "qty": 5}],
+              "price":199
+            },
             title4 : "MotherProduit",
-            description4 : {
-                "name":"poire",
-                "path":"c://Users//terra//Desktop//ProjetIng//Poire.png",
-                "type":"fruit"
-              }
+            description4 :{
+              "name":"poire",
+              "path":"c://Users//terra//Desktop//ProjetIng//Poire.png",
+              "type":"fruit"
+            }
 
       }
     }
 
     render() {
     return (
-        <div>
+        <div style={{"text-align":"left"}}>
             <h2>{this.state.title1}</h2>
             <pre>{JSON.stringify(this.state.description1, null, 1) }</pre>
 
