665a2f4e8050d893e7da78bd95b46a373e72daef
hugoj78 <hugo@macbookprohj.home>
2020-06-08 17:26:59 +0200

    eh oh eh oh

--- IHM/src/pages/Panier.jsx
+++ IHM/src/pages/Panier.jsx
@@ -90,61 +90,51 @@ export class Panier extends Component {
     
     render() {
         if(this.Auth.getToken() === null) {
-                const prod = this.state.products.map((data, key) => (
-                    <tr key={key}>
-                        <td><img width={50} height={50} className="mr-3" src={data.path} alt="Img product" /></td>
-                        <td>{data.name}</td>
-                        <td>In stock</td>
-                        <td><Form.Control type="number" step={1} min={1} max={data.stock} name="qty" value={this.state.numberTemp[key]} onChange={(e) => this.handleChangeStock(key, e)}/></td>
-                        <td>{data.price*this.state.numberTemp[key] || data.price} €</td>
-                        <td><Link to={"/productDetail/" + data._id}><Button className="btn btn-info">Voir produit</Button></Link></td>
-                        <td><Button className="btn btn-sm btn-danger" onClick={(e) => this.deletePanier(key)}>X</Button></td>
-                    </tr>
-                ));
-                const priceTotal = this.state.products.reduce((priceTotal, product, index) => priceTotal + product.price*this.state.numberTemp[index], 0) || this.state.products.reduce((priceTotal, product, index) => priceTotal + product.price, 0);
-                    return (
-                        <Container>
-                            <Row>
-                                    <Table striped hover responsive variant="">
-                                        <thead>
-                                            <tr>
-                                                <th></th>
-                                                <th>Product</th>
-                                                <th>Available</th>
-                                                <th>Quantity</th>
-                                                <th>Price</th>
-                                                <th></th>
-                                                <th></th>
-                                            </tr>
-                                        </thead>
-    
-                                        <tbody>
-                                            {prod}
-                                        </tbody>
-                                    </Table>
-                                    <Col sm={{span: 2, offset: 10}}>
-                                        <p>Total : {priceTotal} €</p>
-                                    </Col>
-                                <Col sm={6}>
-                                    <Link to={"/"}><Button className="btn  btn-success">Continue Shopping</Button></Link> 
+            const prod = this.state.products.map((data, key) => (
+                
+                <tr key={key} >
+                    <td><Link to={"/productDetail/" + data._id}><img width={50} height={50} className="mr-3" src={data.path} alt="Img product" /></Link> </td>
+                    <td>{data.name}</td>
+                    <td>{data.price*this.state.numberTemp[key] || data.price} €</td>
+                    <td><Form.Control type="number" step={1} min={1} max={data.stock} name="qty" value={this.state.numberTemp[key]} onChange={(e) => this.handleChangeStock(key, e)}/></td>
+                    <td><Button className="btn btn-sm btn-danger" onClick={(e) => this.deletePanier(key)}>X</Button></td>
+                </tr>
+               
+            ));
+            const priceTotal = this.state.products.reduce((priceTotal, product, index) => priceTotal + product.price*this.state.numberTemp[index], 0) || this.state.products.reduce((priceTotal, product, index) => priceTotal + product.price, 0);
+                return (
+                    <Container>
+                        <Row>
+                                <Table striped hover responsive variant="" style={{backgroundImage: `url(/img/nook2.png)`, backgroundPosition: 'center', backgroundSize: 'contain', backgroundRepeat: 'no-repeat' }}>
+                                    <thead>
+                                        <tr>
+                                            <th></th>
+                                            <th>Product</th>
+                                            <th>Price</th>
+                                            <th>Quantity</th>
+                                            <th></th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+                                        {prod}
+                                    </tbody>
+                                </Table>
+                                <Col sm={{span: 2, offset: 10}}>
+                                    <p>Total : {priceTotal} €</p>
                                 </Col>
-                                <Col sm={{ span: 1, offset: 2 }}>
-                                    <Button className="btn  btn-success" onClick={(e) => this.createInvoice(priceTotal)} disabled>Checkout</Button>
-                                </Col>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <Col>
-                                    <img
-                                    alt="nook"
-                                    src="/img/nook2.png"
-                                    width="450"
-                                    height="450"
-                                    className="d-inline-block align-top"/>
-                                    <br/><br/><br/>
-                                </Col>
-                            </Row>
-                        </Container>
+                            <Col sm={6}>
+                                <Link to={"/"}><Button className="btn  btn-success">Continue Shopping</Button></Link> 
+                            </Col>
+                            <Col sm={{ span: 1, offset: 2 }}>
+                                <Button className="btn  btn-success" onClick={(e) => this.createInvoice(priceTotal)} disabled>Checkout</Button>
+                            </Col>
+                            <br/>
+                            <br/>
+                            <br/>
+                            
+                        </Row>
+                    </Container>
                 );
         } else {
             const prod = this.state.products.map((data, key) => (
@@ -162,7 +152,7 @@ export class Panier extends Component {
                 return (
                     <Container>
                         <Row>
-                                <Table striped hover responsive variant="" style={{backgroundImage: `url(/img/nook2.png)`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundSize: 'contain', backgroundRepeat: 'no-repeat' }}>
+                                <Table striped hover responsive variant="" style={{backgroundImage: `url(/img/nook2.png)`, backgroundPosition: 'center', backgroundSize: 'contain', backgroundRepeat: 'no-repeat' }}>
                                     <thead>
                                         <tr>
                                             <th></th>
